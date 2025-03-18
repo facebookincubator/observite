896e92154d3b645368bc851668e06d43ac684c69
OkapiCode <1840730+OkapiCode@users.noreply.github.com>
2025-03-18 23:26:11 +0900

    Add JS testing and always output strict

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s', // Match both .js and .ts files in __tests__ directories
+    '**/?(*.)+(spec|test).[jt]s', // Match both .js and .ts files with .spec or .test suffix
+  ],
+  moduleFileExtensions: ['ts', 'js'], // Allow Jest to resolve both .ts and .js files
 };
