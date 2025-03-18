f482a5ab64c4749f1dfb523c28fca0ce09ed0f19
OkapiCode <1840730+OkapiCode@users.noreply.github.com>
2025-03-18 23:18:34 +0900

    Update isPromise.ts
    Only plan to use standard promises so these extra checks are probably un-necessary.

--- src/utils/isPromise.ts
+++ src/utils/isPromise.ts
@@ -5,11 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export function isPromise<T = any>(value: unknown): value is Promise<T> {
-  return (
-    value instanceof Promise ||
-    (typeof value === 'object' &&
-      value !== null &&
-      typeof (value as Promise<T>).then === 'function')
-  );
+export function isPromise<T = unknown>(value: unknown): value is Promise<T> {
+  return value instanceof Promise;
 }
