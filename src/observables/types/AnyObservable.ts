ad841aa9caf8355039ca4780a4a0f2e04de2dd89
OkapiCode <1840730+OkapiCode@users.noreply.github.com>
2025-03-21 23:17:17 +0900

    Update AnyObservable.ts
    Including selectors as a type of observable

--- src/observables/types/AnyObservable.ts
+++ src/observables/types/AnyObservable.ts
@@ -7,15 +7,21 @@
 
 import { AsyncObservable } from '@/AsyncObservable';
 import { AsyncObservableMap } from '@/AsyncObservableMap';
+import { AsyncSelector } from '@/AsyncSelector';
 import { Observable } from '@/Observable';
 import { ObservableMap } from '@/ObservableMap';
+import { Selector } from '@/Selector';
 
 export type AnyObservable<TResolve> =
   | AnySyncObservable<TResolve>
   | AnyAsyncObservable<TResolve>;
 
-export type AnySyncObservable<TResolve> = Observable<TResolve>;
-export type AnyAsyncObservable<TResolve> = AsyncObservable<TResolve>;
+export type AnySyncObservable<TResolve> =
+  | Observable<TResolve>
+  | Selector<TResolve>;
+export type AnyAsyncObservable<TResolve> =
+  | AsyncObservable<TResolve>
+  | AsyncSelector<TResolve>;
 
 export type AnyObservableMap<TKey, TResolve> =
   | AnySyncObservableMap<TKey, TResolve>
